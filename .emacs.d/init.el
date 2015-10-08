34740f6865dbc52c11e5f80dde4a7cf312fe6efb
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-10-07 22:16:50 -0400

    removed plugins from vc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,6 +27,11 @@
     '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
   (add-hook 'after-init-hook #'global-flycheck-mode))
 (use-package diff-hl)
+(use-package smart-mode-line
+  :config
+  (sml/setup)
+  (sml/apply-theme 'dark))
+
 
 
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -75,3 +80,17 @@ buffer is not visiting a file."
 
 (provide '.emacs)
 ;;; .emacs ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
